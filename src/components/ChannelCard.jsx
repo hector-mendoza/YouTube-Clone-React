6132214548413dbf8ff3b5d9b2a6248913605ab4
hector-mendoza <hec1702@gmail.com>
2023-04-07 10:47:29 -0600

    Styling fixes to the Channel Card (Image)

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -18,7 +18,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
             }}>
                 <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
                     alt={channelDetail?.snippet?.title}
-                    sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+                    sx={{ borderRadius: '50%', height: '180px', width: '180px', mx: 'auto', mb: 2, border: '1px solid #e3e3e3' }}
                 />
                 <Typography variant="h6">
                     {channelDetail?.snippet?.title}
